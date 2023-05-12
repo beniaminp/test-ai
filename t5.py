a5fa069728bd177835e1c6d0aec2e469b8425f18
Dragos P <beniaminpantiru@gmail.com>
2023-05-12 14:19:40 +0000

    dsadsa

--- t5.py
+++ t5.py
@@ -1,11 +1,20 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+import torch
 
-tokenizer = AutoTokenizer.from_pretrained("t5-small")
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
-
-inputs = tokenizer("Hello world!", return_tensors="pt")
-
-outputs = model(**inputs)
-
-decoded = tokenizer.decode(outputs[0])
-print(decoded)
+model_name = 't5-small'
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+"""
+prompt = "Once upon a time"
+input_ids = tokenizer.encode(prompt, return_tensors='pt')
+output = model.generate(input_ids)
+generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+print(generated_text)
+"""
+passage = "This is the text from which you want to extract information."
+question = "What is the main idea?"
+input_text = "question: " + question + " context: " + passage
+input_ids = tokenizer.encode(input_text, return_tensors='pt')
+output = model.generate(input_ids)
+answer = tokenizer.decode(output[0], skip_special_tokens=True)
+print(answer)
