436b8cdbfd528f5df5a352b603f5cefc851a54c3
Dragos P <beniaminpantiru@gmail.com>
2023-05-12 14:21:07 +0000

    dsadsa

--- t5.py
+++ t5.py
@@ -1,5 +1,15 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
+from PyPDF2 import PdfReader
+
+# creating a pdf reader object
+reader = PdfReader('inv_2.pdf')
+  
+# getting a specific page from the pdf file
+page = reader.pages[0]
+  
+# extracting text from page
+text = page.extract_text()
 
 model_name = 't5-small'
 tokenizer = T5Tokenizer.from_pretrained(model_name)
@@ -11,8 +21,8 @@ output = model.generate(input_ids)
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 print(generated_text)
 """
-passage = "This is the text from which you want to extract information."
-question = "What is the main idea?"
+passage = text
+question = "What is the time and date of the bookig?"
 input_text = "question: " + question + " context: " + passage
 input_ids = tokenizer.encode(input_text, return_tensors='pt')
 output = model.generate(input_ids)
